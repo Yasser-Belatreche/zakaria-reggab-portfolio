202251d0986f3beea8ecd74f375b9eede6180045
yasserBelatreche <yasserbelatreche000@gmail.com>
2021-05-12 15:24:08 +0100

    countup animation added to the achivements

--- src/components/achivement/Achivement.js
+++ src/components/achivement/Achivement.js
@@ -1,13 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import CountUp from "react-countup"
+import VisibilitySensor from "react-visibility-sensor"
 
 const Achivement = ({ icon, text, number }) => {
+  const [viewPortEntered, setViewPortEntered] = useState(false)
+
   return (
     <div className="achivement">
       <div className="icon">
         <img src={icon} alt="icon" />
       </div>
       <div className="text">
-        <h1 className="number">{number}</h1>
+        <h1 className="number">
+          <CountUp start={viewPortEntered ? null : 0} end={number} delay={0.3}>
+            {({ countUpRef }) => (
+              <VisibilitySensor
+                active={!viewPortEntered}
+                onChange={isVisible => {
+                  if (isVisible) {
+                    setViewPortEntered(true)
+                  }
+                }}
+              >
+                <span ref={countUpRef} />
+              </VisibilitySensor>
+            )}
+          </CountUp>
+        </h1>
         <p>{text}</p>
       </div>
     </div>
